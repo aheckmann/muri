85124ed76c3ca698f21c9ab68322af78d66bd574
Jarom Loveridge <jaroml@gradecam.com>
2018-04-12 17:04:57 -0600

    Add TypeScript definition.

---
+++ index.d.ts
@@ -0,0 +1,26 @@
+// Type definitions for muri
+// Project: muri
+// Definitions by: Jarom Loveridge https://github.com/jloveridge
+export = Muri;
+
+declare function Muri(uri: string): Muri.ParsedUri;
+
+declare namespace Muri {
+    interface ParsedUri {
+        db: string;
+        hosts: Host[];
+        auth?: {user: string; pass: string;};
+        options?: any;
+    }
+
+    interface DefaultHost {
+        host: string;
+        port: number;
+    }
+
+    interface SocketHost {
+        ipc: string;
+    }
+
+    type Host = DefaultHost | SocketHost;
+}
